526e792717c3526b04561366414a73caa2487b78
xxHRUSHIKESHxx <khrushikesh2002@gmail.com>
2023-07-12 18:50:51 +0530

    uploading youtube clone 1st

---
+++ src/components/Feed.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { fetchFromAPI ,axios} from '../utils/fetchFromAPI';
+import { Sidebar, Videos } from './';
+import { FaceRetouchingNatural } from '@mui/icons-material';
+
+
+const Feed = () => {
+
+  const [selectedcategory, setselectedcategory] = useState(['New']);
+
+  const [videos, setVideos] = useState([]);
+
+ useEffect(()=>{
+
+  fetchFromAPI(`search?part=snippet&q=${selectedcategory}`) 
+  .then((data) => {setVideos(data.items)})
+  .catch((error) => {
+    console.log(error)
+  })
+ }, [selectedcategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: '92vh' },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+        selectedCategory={selectedcategory}
+        setselectedcategory ={setselectedcategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          copyright 2022 jsm media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: "white",
+          }}
+        >
+          {selectedcategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
+  
