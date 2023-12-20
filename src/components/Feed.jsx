7f3c4e1ea61a0e80870511bf95ecd1cd53eb96a9
xxHRUSHIKESHxx <khrushikesh2002@gmail.com>
2023-12-21 00:42:49 +0530

    changed logo and copyright

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -38,7 +38,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          CopyRight 2023 xxHRUSHIKESHxx
+         -- CopyRight BSH Group --
         </Typography>
       </Box>
 
